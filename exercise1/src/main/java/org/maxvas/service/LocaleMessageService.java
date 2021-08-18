cf413ca2c05a5d09d9c2e0c718e75e0ab101f24d
Maxim <vasiliev.maxim@gmail.com>
2021-08-18 21:10:55 +0300

    Домашнее задание 3. Исправление по замечаниям #2.

--- exercise1/src/main/java/org/maxvas/service/LocaleMessageService.java
+++ exercise1/src/main/java/org/maxvas/service/LocaleMessageService.java
@@ -1,5 +1,6 @@
 package org.maxvas.service;
 
+import lombok.AllArgsConstructor;
 import org.maxvas.conf.LocaleConfiguration;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
@@ -7,19 +8,15 @@ import org.springframework.stereotype.Service;
 import java.util.Locale;
 
 @Service
+@AllArgsConstructor
 public class LocaleMessageService {
 
-    private final Locale locale;
+    private final LocaleConfiguration localeConfiguration;
     private final MessageSource messageSource;
 
-    public LocaleMessageService(LocaleConfiguration localeConfiguration,
-                                MessageSource messageSource) {
-        this.messageSource = messageSource;
-        String localeTag = localeConfiguration.getLocale();
-        this.locale = Locale.forLanguageTag(localeTag);
-    }
-
     public String getMessage(String s, Object... params) {
+        String localeTag = localeConfiguration.getLocale();
+        Locale locale = Locale.forLanguageTag(localeTag);
         return messageSource.getMessage(s, params, locale);
     }
 }
